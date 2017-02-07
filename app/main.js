da7282f5cfd4f58cdda0b814ad8b1860528b2b05
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-02-08 10:30:30 +1100

    start to implement solution for different size game board

--- app/main.js
+++ app/main.js
@@ -11,38 +11,29 @@ var boardSize = 3;
 
 // hard coded turn varieble so each game will begin with player one. This var will change between players on each turn
 var whosTurn = playerOne;
-var boardArray = [
-["", "", "", "", ""],
-["", "", "", "", ""],
-["", "", "", "", ""],
-["", "", "", "", ""],
-["", "", "", "", ""]
-];
-
-// storing the innerText content in variables to be able to check of a player has 3 in a row - check for winner
-
-// var squareOne;
-// var squareTwo;
-// var squareThree;
-// var squareFour;
-// var squareFive;
-// var squareSix;
-// var squareSeven;
-// var squareEight;
-// var squareNine;
+var virtualBoard = {
+  0:"", 1:"", 2:"", 3:"", 4:"",
+  5:"", 6:"", 7:"", 8:"", 9:"",
+  10:"", 11:"", 12:"", 13:"", 14:"",
+  15:"", 16:"", 17:"", 18:"", 19:"",
+  20:"", 21:"", 22:"", 23:"", 24:""
+};
 
 // function to create the game board. Defaults to 3 x 3 when the page is loaded
 var createGameBoard = function() {
   board.innerHTML = "";
+  var counter = 0;
   for (var i = 0; i < boardSize; i++) {
     var row = document.createElement("tr");
     board.appendChild(row);
     for (var j = 0; j < boardSize; j++) {
       var cell = document.createElement("td");
       row.appendChild(cell);
+      cell.setAttribute("id", counter);
+      counter+=1;
     }
   }
-}
+};
 
 // excecuteTurn changes innerText of the HTML tags "td" based on the users click location. Also switches between "X" and "O" each turn and checks that the clicked locations innerText is empty to ensure a previously clicked location cannot be over-written
 var a;
@@ -52,55 +43,24 @@ var excecuteTurn = function(e) {
     if (e.target.innerText === "") {
         e.target.innerText = whosTurn;
         boardArray[e.target.parentNode.rowIndex][e.target.cellIndex] = whosTurn;
+        debugger;
         if (whosTurn === playerOne) {
             whosTurn = playerTwo;
         } else {
             whosTurn = playerOne;
         }
-        // updates clicked squares variable to the innerText content each time the function fires
-        squareOne = document.getElementById("1").innerText;
-        squareTwo = document.getElementById("2").innerText;
-        squareThree = document.getElementById("3").innerText;
-        squareFour = document.getElementById("4").innerText;
-        squareFive = document.getElementById("5").innerText;
-        squareSix = document.getElementById("6").innerText;
-        squareSeven = document.getElementById("7").innerText;
-        squareEight = document.getElementById("8").innerText;
-        squareNine = document.getElementById("9").innerText;
-
-    }
-    // if statement to check if a player has 3 in a row - virtical, horizontal or diagonal and triggers the appropriate function to update DOM based on which player one
-    if (squareOne === "X" && squareTwo === "X" && squareThree === "X" || squareFour === "X" && squareFive === "X" && squareSix === "X" || squareSeven === "X" && squareEight === "X" && squareNine === "X" || squareOne === "X" && squareFour === "X" && squareSeven === "X" || squareTwo === "X" && squareFive === "X" && squareEight === "X" || squareThree === "X" && squareSix === "X" && squareNine === "X" || squareOne === "X" && squareFive === "X" && squareNine === "X" || squareThree === "X" && squareFive === "X" && squareSeven === "X") {
-        playerOneWon();
-    }
-    if (squareOne === "O" && squareTwo === "O" && squareThree === "O" || squareFour === "O" && squareFive === "O" && squareSix === "O" || squareSeven === "O" && squareEight === "O" && squareNine === "O" || squareOne === "O" && squareFour === "O" && squareSeven === "O" || squareTwo === "O" && squareFive === "O" && squareEight === "O" || squareThree === "O" && squareSix === "O" && squareNine === "O" || squareOne === "O" && squareFive === "O" && squareNine === "O" || squareThree === "O" && squareFive === "O" && squareSeven === "O") {
-        playerTwoWon();
-    }
-    else if (squareOne && squareTwo && squareThree && squareFour && squareFive && squareSix && squareSeven && squareEight && squareNine === "X" || squareOne && squareTwo && squareThree && squareFour && squareFive && squareSix && squareSeven && squareEight && squareNine === "O") {
-        draw();
     }
   } if (gameStatus === "completed") {
     return;
   }
 }
 
-var winnerCheckHorizontal = function() {
+var checkForWinner = function() {
   for (var i = 0; i < boardSize; i++){
     for (var j = 0; j < boardSize; j++){
       console.log(boardArray[i][j]);
     }
   }
-}
-
-var winnerCheckVirtical = function() {
-  for (var i = 0; i < boardSize; i++){
-    for (var j = 0; j < boardSize; j + 3){
-      console.log(boardArray[i][j]);
-    }
-  }
-}
-
-var winnerCheckDiagonal = function() {
 }
 
 var playerOneWon = function() {
